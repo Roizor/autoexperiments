196aa3704483f72b0ea499fe7359c3d514f7d1b6
Roizor <68617740+Roizor@users.noreply.github.com>
2022-08-30 06:56:47 -0500

    it would be so awesome
    gonna be honest i don't understand 50% of this code. i don;t even know if the unload funcion works LOL

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = class AutoExperiments extends Plugin {
 		let wpRequire;
 		window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
 		let mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.default?.isDeveloper !== "undefined")
+		window.autoexp = mod;
 		let usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
 		let nodes = Object.values(mod.exports.default._dispatcher._actionHandlers._dependencyGraph.nodes);
 		try {
@@ -17,6 +18,18 @@ module.exports = class AutoExperiments extends Plugin {
 	}
 
 	pluginWillUnload() {
-		
+		let wpRequire;
+		window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
+		let mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.default?.isDeveloper !== "undefined")
+		window.autoexp = mod;
+		let usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
+		let nodes = Object.values(mod.exports.default._dispatcher._actionHandlers._dependencyGraph.nodes);
+		try {
+			nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user: {flags: 1}})
+		} catch (e) {}
+		let oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
+		usermod.exports.default.__proto__.getCurrentUser = () => ({hasFlag: () => false})
+		nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
+		usermod.exports.default.__proto__.getCurrentUser = oldGetUser
 	}
 };
